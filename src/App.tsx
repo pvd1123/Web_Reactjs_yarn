ed8038557b6c7495eab39f74b61bb55991bcbabb
PhatDT2 <phathuynh0106@gmail.com>
2023-04-11 20:10:47 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { buffer } from 'stream/consumers';
+
+function Button(props: {label: string}){
+  return(
+    <button type='button'>{props.label}</button>
+  )
+}
+
+function Link(){
+  return(
+    <a href="">Home</a>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Phat</h1>
+      <Button label='Add to Cart'/>
+      <Button label='Delete'/>
     </div>
   );
 }
