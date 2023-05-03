817d05e57294e6efc7411f0a4ae122799d01ceba
PhatDT2 <phathuynh0106@gmail.com>
2023-05-03 22:17:26 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { buffer } from 'stream/consumers';
+// import Navbar from '../src/components/navbar'
+import { Routes, Route } from 'react-router-dom';
+// import {Container} from 'react-bootstrap'
 
 
 function App(){
   return(
   <div className='App'>
-    app
+    
   </div>
   )
 }
